cea264a5882b3715324f1301220df2d7357f8753
Isiah Manns <isiah.michael1018@gmail.com>
2023-08-02 11:14:00 -0400

    Placeholder value for ScoreMeter

--- Movies/Movies/Views/ScoreMeterSUI.swift
+++ Movies/Movies/Views/ScoreMeterSUI.swift
@@ -2,13 +2,21 @@ import SwiftUI
 
 struct ScoreMeterSUI: View {
     @StateObject var model: ScoreMeterModel
-    let gradient = Gradient(colors: [.red, .orange, .yellow, .green])
+    private let gradient = Gradient(colors: [.red, .orange, .yellow, .green])
+    private var currentValueLabel: String {
+        guard let value = model.value
+        else { return "..."}
+
+        return value == 0
+        ? "NR"
+        : "\(Int(value * 100))"
+    }
 
     var body: some View {
-        Gauge(value: model.value, in: 0...1) {
-            Text("Score: \(model.value)")
+        Gauge(value: model.value ?? 0, in: 0...1) {
+            Text("Score: \(currentValueLabel)")
         } currentValueLabel: {
-            Text("\(Int(model.value * 100))")
+            Text(currentValueLabel)
         } minimumValueLabel: {
             Text("0")
         } maximumValueLabel: {
@@ -26,5 +34,11 @@ struct ScoreMeterSUI_Previews: PreviewProvider {
 }
 
 class ScoreMeterModel: ObservableObject {
-    @Published var value: Float = 0
+    @Published var value: Float? = nil {
+        didSet {
+            if let value {
+                precondition((0...1).contains(value))
+            }
+        }
+    }
 }
