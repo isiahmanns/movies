ba3df2b7450433b43f527f9c8a6826f60d5a528d
Isiah Manns <isiah.michael1018@gmail.com>
2023-08-06 12:11:40 -0400

    Toggle Watchlist state on main actor

--- Movies/Movies/ViewControllers/StateTogglingViewController.swift
+++ Movies/Movies/ViewControllers/StateTogglingViewController.swift
@@ -1,5 +1,6 @@
 protocol ViewControllerState {}
 
+@MainActor
 protocol StateTogglingViewController {
     associatedtype ViewState = ViewControllerState
     func toggleState(_: ViewState)
--- Movies/Movies/ViewModels/SavedMoviesListViewModel.swift
+++ Movies/Movies/ViewModels/SavedMoviesListViewModel.swift
@@ -1,6 +1,7 @@
 import Foundation
 import UIKit
 
+@MainActor
 class SavedMoviesListViewModel {
     var movies: [Movie] = [] {
         didSet {
